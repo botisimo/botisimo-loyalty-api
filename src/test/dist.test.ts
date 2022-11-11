3054c2073c20c1c4c10da60038c01ef4f3e0d669
Samuel Dillow <tarwich@gmail.com>
2022-11-11 13:53:20 -0600

    Tidy things up

--- src/test/dist.test.ts
+++ src/test/dist.test.ts
@@ -1,21 +1,40 @@
-// import { BotisimoLoyaltyApi } from '../../';
-// import { MockServer } from './server.mock';
-// import fetch from 'node-fetch';
+//
+// Test the build files in dist/ folder to make sure they are working
+//
+import { BotisimoLoyaltyApi } from '../../../botisimo-loyalty-api';
+import { MockServer } from './server.mock';
+import fetch from 'node-fetch';
 
 describe('BotisimoLoyaltyApi', () => {
+  const makeServer = () => new MockServer();
+  const makeApi = (server: MockServer) => {
+    const api = new BotisimoLoyaltyApi('test', {
+      fetch,
+    });
+    api.baseUrl = server.url;
+    return api;
+  };
+  let server: MockServer;
+  let api: BotisimoLoyaltyApi;
+
+  beforeEach(() => {
+    server = makeServer();
+    api = makeApi(server);
+  });
+
+  afterEach(() => {
+    server.destroy();
+  });
+
   it('should make a basic api call', async () => {
-    // const server = new MockServer();
-    // server.post = jest.fn().mockResolvedValue({ token: 'test' });
-    // const api = new BotisimoLoyaltyApi(server.url, {
-    //   fetch,
-    // });
-    // const promise = api.login({ email: 'test', password: 'test' });
-    // await expect(promise).resolves.toEqual({ token: 'test' });
-    // expect(server.post).toHaveBeenCalledWith(
-    //   expect.objectContaining({
-    //     url: '/login',
-    //     body: { email: 'test', password: 'test' },
-    //   }),
-    // );
+    server.post = jest.fn().mockResolvedValue({ token: 'test' });
+    const promise = api.login({ email: 'test', password: 'test' });
+    await expect(promise).resolves.toEqual({ token: 'test' });
+    expect(server.post).toHaveBeenCalledWith(
+      expect.objectContaining({
+        url: '/login',
+        body: { email: 'test', password: 'test' },
+      }),
+    );
   });
 });
